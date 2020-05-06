9a73acab6991e77ca1aed68281789fb23435204f
streemua <streembodyua@gmail.com>
2020-05-06 20:14:29 +0300

    Updated market tests

--- cypress/integration/binance/market_tests.js
+++ cypress/integration/binance/market_tests.js
@@ -1,7 +1,11 @@
 
 describe('Binance Market Tests', () => {
-    it('Verify_Market_Search_Add_Functionality', () => {
+    it('Verify_Market_Search_Add_Favorites', () => {
         cy.visit('https://www.binance.com/en/markets')
-        cy.title().should('eq','Bitcoin Exchange | Cryptocurrency Exchange | Binance')
+        cy.get('input[placeholder="Search coin name"]').clear().type('ETH')
+        cy.get('input[placeholder="Search coin name"]').clear()
+        cy.xpath(".//a [@href='/en/trade/ETH_BTC']//* [name()='svg']").click()
+        cy.xpath(".//a [@href='/en/trade/BNB_BTC']//* [name()='svg']").click()
+        cy.xpath(".//div [text()='Favorites']").click()
     })
 })
